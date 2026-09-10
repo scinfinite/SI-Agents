8536cb7ec7be885cfdcc6811b1436ca915568a04
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-10 10:50:07 +0530

    feat: add open-source intelligence models

---
+++ core/open_source/__init__.py
@@ -0,0 +1,23 @@
+from core.open_source.models import (
+    HealthAssessment,
+    IssueSummary,
+    LicenseAssessment,
+    OpenSourceSnapshot,
+    PullRequestSummary,
+    ReleaseSummary,
+    RepositorySummary,
+    SecurityAdvisory,
+)
+from core.open_source.registry import OpenSourceRegistry
+
+__all__ = [
+    "HealthAssessment",
+    "IssueSummary",
+    "LicenseAssessment",
+    "OpenSourceRegistry",
+    "OpenSourceSnapshot",
+    "PullRequestSummary",
+    "ReleaseSummary",
+    "RepositorySummary",
+    "SecurityAdvisory",
+]
