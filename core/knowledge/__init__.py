77e821f05f4c0d23dd18109c2a022e0c9e74210a
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-10 10:33:00 +0530

    feat(knowledge): add technical knowledge domain

---
+++ core/knowledge/__init__.py
@@ -0,0 +1,15 @@
+from core.knowledge.models import (
+    KnowledgeEntry,
+    KnowledgeKind,
+    KnowledgeSource,
+    KnowledgeStatus,
+)
+from core.knowledge.registry import KnowledgeRegistry
+
+__all__ = [
+    "KnowledgeEntry",
+    "KnowledgeKind",
+    "KnowledgeSource",
+    "KnowledgeStatus",
+    "KnowledgeRegistry",
+]
