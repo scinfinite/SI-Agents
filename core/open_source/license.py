773395dfc703c94cf440d73a3456b3a4095129cc
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-10 11:02:38 +0530

    fix: scope Ruff import-order exception to license analyzer

--- core/open_source/license.py
+++ core/open_source/license.py
@@ -1,4 +1,4 @@
-import core.open_source.models as models
+from core.open_source import models  # noqa: I001
 
 
 # Conservative identifiers only. This is not legal advice and does not replace license review.
