ff64e48bb31da87739cdc65082d96db76dbdbca5
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-10 09:35:55 +0530

    Export capability intelligence API

--- core/capabilities/__init__.py
+++ core/capabilities/__init__.py
@@ -1,1 +1,26 @@
-"""Capability registry primitives."""
+"""Capability intelligence primitives."""
+
+from core.capabilities.benchmark import CapabilityBenchmark, CapabilityBenchmarkStore
+from core.capabilities.discovery import CapabilityDiscovery, CapabilityDiscoveryIndex
+from core.capabilities.health import CapabilityHealth, CapabilityHealthTracker
+from core.capabilities.lifecycle import CapabilityLifecycle, CapabilityProposal
+from core.capabilities.models import Capability, CapabilityStatus
+from core.capabilities.registry import CapabilityRegistry
+from core.capabilities.versioning import CapabilityVersionManager, bump_version, validate_version
+
+__all__ = [
+    "Capability",
+    "CapabilityBenchmark",
+    "CapabilityBenchmarkStore",
+    "CapabilityDiscovery",
+    "CapabilityDiscoveryIndex",
+    "CapabilityHealth",
+    "CapabilityHealthTracker",
+    "CapabilityLifecycle",
+    "CapabilityProposal",
+    "CapabilityRegistry",
+    "CapabilityStatus",
+    "CapabilityVersionManager",
+    "bump_version",
+    "validate_version",
+]
