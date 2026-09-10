388acb65704c600bfdcd586494f2e790532dec96
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-10 15:44:27 +0530

    feat: expose Codespaces runtime

--- core/environments/__init__.py
+++ core/environments/__init__.py
@@ -1,5 +1,6 @@
 """Environment detection and runtime readiness primitives."""
 
+from core.environments.codespace import CodespaceConfig, CodespaceRuntime
 from core.environments.models import (
     EnvironmentKind,
     EnvironmentReport,
@@ -9,6 +10,8 @@ from core.environments.models import (
 from core.environments.termux import TermuxConfig, TermuxRuntime
 
 __all__ = [
+    "CodespaceConfig",
+    "CodespaceRuntime",
     "EnvironmentKind",
     "EnvironmentReport",
     "EnvironmentStatus",
