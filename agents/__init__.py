e9b5aeddbf3ab66a188982a1d2dc08a5b80f2583
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-11 22:26:36 +0530

    feat: export Phase 49 builder contracts

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,6 +1,10 @@
 from agents.base import AgentContext, AgentResult, AgentSpec
+from agents.builder import AgentDefinition, AgentRegistry, BuilderError, ResourceLimits, TeamBuilder, TeamDefinition
 from agents.debugger import DebuggerAgent
 from agents.developer import DeveloperAgent
 from agents.tester import TesterAgent
 
-__all__ = ["AgentContext", "AgentResult", "AgentSpec", "DebuggerAgent", "DeveloperAgent", "TesterAgent"]
+__all__ = [
+    "AgentContext", "AgentDefinition", "AgentRegistry", "AgentResult", "AgentSpec", "BuilderError",
+    "DebuggerAgent", "DeveloperAgent", "ResourceLimits", "TeamBuilder", "TeamDefinition", "TesterAgent",
+]
