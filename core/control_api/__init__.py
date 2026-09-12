3970f848cd2dcab61e997321835ccf1440af8c80
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-12 19:43:15 +0530

    feat(web): export web control server

--- core/control_api/__init__.py
+++ core/control_api/__init__.py
@@ -10,8 +10,10 @@ from core.control_api.openapi import document
 from core.control_api.server import ControlApiHandler, create_server
 from core.control_api.service import ControlApiService
 from core.control_api.waiting_routes import install_waiting_routes
+from core.control_api.web import WebControlHandler, create_web_control_server
 
 install_waiting_routes(ControlApiHandler)
+install_waiting_routes(WebControlHandler)
 
 __all__ = [
     "API_VERSION",
@@ -22,6 +24,8 @@ __all__ = [
     "ControlApiService",
     "RunRecord",
     "RunStatus",
+    "WebControlHandler",
     "create_server",
+    "create_web_control_server",
     "document",
 ]
