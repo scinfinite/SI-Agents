d6fbe431621fcf0c0e717a92736401503fb5ea6a
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-12 13:32:44 +0530

    feat(phase56): export intelligent routing contracts

--- core/provider_intelligence/__init__.py
+++ core/provider_intelligence/__init__.py
@@ -1,6 +1,18 @@
 """Model and provider intelligence primitives."""
 
 from core.provider_intelligence.circuit import CircuitBreaker
+from core.provider_intelligence.intelligent_router import (
+    BudgetExceededError,
+    BudgetLedger,
+    IntelligentRouter,
+    RouteCandidate,
+    RouteEvidence,
+    RoutePolicy,
+    RoutingError,
+    TaskComplexity,
+    TaskRequirements,
+    infer_complexity,
+)
 from core.provider_intelligence.models import (
     Capability,
     ModelProfile,
@@ -26,8 +38,11 @@ from core.provider_intelligence.registry import ProviderRegistry
 from core.provider_intelligence.router import ModelRouter
 
 __all__ = [
+    "BudgetExceededError",
+    "BudgetLedger",
     "Capability",
     "CircuitBreaker",
+    "IntelligentRouter",
     "ModelProfile",
     "ModelRouter",
     "OmniRouteClient",
@@ -42,6 +57,13 @@ __all__ = [
     "ProviderProfile",
     "ProviderRegistry",
     "QuotaSnapshot",
+    "RouteCandidate",
+    "RouteEvidence",
+    "RoutePolicy",
     "RoutingDecision",
+    "RoutingError",
     "RoutingRequest",
+    "TaskComplexity",
+    "TaskRequirements",
+    "infer_complexity",
 ]
