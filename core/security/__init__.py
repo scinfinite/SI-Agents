72ef680cecdbf3f38521675f2c10ed4a356fb3d7
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-12 14:02:42 +0530

    feat(phase57): add security platform

---
+++ core/security/__init__.py
@@ -0,0 +1,31 @@
+"""Phase 57 security platform contracts."""
+
+from core.security.platform import (
+    AuditEvent,
+    AuthorizationRequest,
+    AuthorizationResult,
+    EgressPolicy,
+    Identity,
+    PermissionGrant,
+    PolicyVersion,
+    SecretScanner,
+    SecurityPolicy,
+    SecurityPlatform,
+    SecurityToken,
+    TrustBoundary,
+)
+
+__all__ = [
+    "AuditEvent",
+    "AuthorizationRequest",
+    "AuthorizationResult",
+    "EgressPolicy",
+    "Identity",
+    "PermissionGrant",
+    "PolicyVersion",
+    "SecretScanner",
+    "SecurityPolicy",
+    "SecurityPlatform",
+    "SecurityToken",
+    "TrustBoundary",
+]
