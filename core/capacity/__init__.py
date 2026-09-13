4e0d08584abaf6aa441781564f7547a684e07cdb
SC Infinite <sc.infinite.work.11@gmail.com>
2026-09-13 10:05:44 +0530

    feat: add capacity safety policy

---
+++ core/capacity/__init__.py
@@ -0,0 +1,5 @@
+"""Capacity-aware execution safety for local and remote SI-Agents runtimes."""
+
+from core.capacity.policy import CapacityDecision, CapacityLevel, CapacityPolicy, CapacityTarget
+
+__all__ = ["CapacityDecision", "CapacityLevel", "CapacityPolicy", "CapacityTarget"]
